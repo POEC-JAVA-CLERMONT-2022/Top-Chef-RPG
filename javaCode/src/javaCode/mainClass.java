c68d8c56f8324fe39ca1e186078113883e6092b9
manufrom63 <100273523+manufrom63@users.noreply.github.com>
2022-03-09 13:54:48 +0100

    add java project

---
+++ javaCode/src/javaCode/mainClass.java
@@ -0,0 +1,10 @@
+package javaCode;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javaCode/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dev
+ *
+ */
+module javaCode {
+}
