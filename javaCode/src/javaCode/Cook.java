63fe46bd494bf09330aa13e38b07b5bda479d790
manufrom63 <100273523+manufrom63@users.noreply.github.com>
2022-03-17 09:26:25 +0100

    ecriture des classes

--- javaCode/src/javaCode/Cook.java
+++ javaCode/src/javaCode/Cook.java
@@ -1,5 +1,31 @@
 package javaCode;
 
+import java.util.ArrayList;
+
 public class Cook {
+	//déclaration des variables de cook
+	private String name;
+	private Character gender;
+	private int experience;
+	private int dexterity;
+	private int strength;
+	private int creativity;
+	private int luck;
+	private ArrayList<Lesson> lessons;
+	private ArrayList<Ingredient> ingredients;
+	//private ArrayList<Tool> tools;
+	
+	
+	// methode de création du joueur avec ses variables à 0
+	public Cook (String name)
+	{
+		this.name = name;
+		this.experience= 0;
+		this.creativity=0;
+		this.dexterity=0;
+		this.strength=0;
+		this.luck=0;
+		//rajouter appel vers méthodde instanciation listes. 
+	}
 	
 }
---
+++ javaCode/src/javaCode/Ingredient.java
@@ -0,0 +1,40 @@
+package javaCode;
+
+public class Ingredient {
+	// déclaration des variables
+	private String name;
+	private int quantity;
+	
+	
+	// constructeur par défaut mets la quantité à-1 pour les ingredients non encore débloqués
+	public Ingredient(String ingredientName)
+	{
+		this.name = ingredientName;
+		this.quantity = -1;
+	}
+	
+	// fonction pour obtenir la quantité d'ingredient 
+	public int ingredientQuantity ()
+	{
+		return this.quantity;
+	}
+	
+	// fonction pour changer la quantité de l'ingrédient
+	public void setQuantity (int quantityUpdate)
+	{
+		if ((this.quantity + quantityUpdate)>=0)
+			this.quantity += quantityUpdate;
+		else 
+		{
+			// en cas de mise à jour impossible car update erroné
+			System.out.println("erreur de quantité, la quantité deviendrait négative");
+		}
+	}
+	
+	
+	
+	
+	
+	
+
+}
