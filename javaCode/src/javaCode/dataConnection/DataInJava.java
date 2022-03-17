424ef6bc106b4f9e38762096ab08c2867b01992c
fabien <100273019+agalyn63@users.noreply.github.com>
2022-03-17 15:08:33 +0100

    recettes et ingredients

--- javaCode/src/javaCode/dataConnection/DataInJava.java
+++ javaCode/src/javaCode/dataConnection/DataInJava.java
@@ -44,12 +44,51 @@ public class DataInJava implements InterfaceData {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	
+	
 	@Override
 	public ArrayList<Recipe> getRecipe() {
-		Recipe recette1 = new Recipe()
+		ArrayList<Recipe> recipes = new ArrayList<Recipe>(); 
+		Ingredient pommes = new Ingredient("pomme");
+		Ingredient pommeLoots = new Ingredient("pomme");
+		pommes.setQuantity(2);
+		pommeLoots.setQuantity(10);
+		Ingredient pdt = new Ingredient("pomme de terre");
+		Ingredient pdtLoot = new Ingredient("pomme de terre");
+		pdt.setQuantity(5);
+		pdtLoot.setQuantity(10);
+		Ingredient farine = new Ingredient("farine");
+		Ingredient farineLoot = new Ingredient("farine");
+		farine.setQuantity(3);
+		farineLoot.setQuantity(8);
+		ArrayList<Ingredient> listeIngredientLoot = new ArrayList<Ingredient>();
+		listeIngredientLoot.add(pdtLoot);
+		ArrayList<Ingredient> listeIngredient = new ArrayList<Ingredient>();
+		
+		ArrayList<Ingredient> listeIngredient2= new ArrayList<Ingredient>();
+		listeIngredient2.add(pdt);
+		ArrayList<Ingredient> listeIngredientLoot2 = new ArrayList<Ingredient>();
+		listeIngredientLoot2.add(farineLoot);
+		listeIngredientLoot2.add(pommeLoots);
+		
+		ArrayList<Ingredient> listeIngredient3= new ArrayList<Ingredient>();
+		listeIngredient3.add(farine);
+		listeIngredient3.add(pommes);
+		ArrayList<Ingredient> listeIngredientLoot3 = new ArrayList<Ingredient>();
+		listeIngredientLoot3.add(farineLoot);
+		listeIngredientLoot3.add(pommeLoots);
+		listeIngredientLoot3.add(pdtLoot);
+		
+		
+		Recipe recette1 = new Recipe("tourte", 10, 10, 10, 5, listeIngredient , listeIngredientLoot); 
+		Recipe recette2 = new Recipe("frites", 12, 12, 12, 2, listeIngredient2, listeIngredientLoot2);
+		Recipe recette3 = new Recipe("cake", 15, 15, 16, 9, listeIngredient3, listeIngredientLoot3);
 		// TODO Auto-generated method stub
-		return null;
+		recipes.add(recette1);
+		recipes.add(recette2);
+		recipes.add(recette3);
+		return recipes ;
 	}
 
 
