0fd31327e962e1bf7b39fb0e8fab7a36d0a4409b
manufrom63 <100273523+manufrom63@users.noreply.github.com>
2022-03-18 09:03:36 +0100

    Update DataInJava.java

--- javaCode/src/javaCode/dataConnection/DataInJava.java
+++ javaCode/src/javaCode/dataConnection/DataInJava.java
@@ -9,19 +9,19 @@ import javaCode.Recipe;
 import javaCode.User;
 
 public class DataInJava implements InterfaceData {
-	
-	
+
+
 
 	@Override
 	public void saveCook(Cook cook) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Cook loadCook(String nameCook) {
 		// TODO Auto-generated method stub
-		
+
 		Cook neoCook = new Cook(nameCook);
 		if (nameCook.equals("robert"))
 		{
@@ -36,7 +36,7 @@ public class DataInJava implements InterfaceData {
 			ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
 			ingredients.add(pomme);
 			neoCook.setIngredients(ingredients);
-			
+
 		}
 		return neoCook;
 	}
@@ -46,13 +46,12 @@ public class DataInJava implements InterfaceData {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
-	
+
+
+
 	@Override
 	public ArrayList<Recipe> getRecipe() {
 
-<<<<<<< Updated upstream
 		ArrayList<Recipe> recipes = new ArrayList<Recipe>(); 
 		Ingredient pommes = new Ingredient("pomme");
 		Ingredient pommeLoots = new Ingredient("pomme");
@@ -69,13 +68,13 @@ public class DataInJava implements InterfaceData {
 		ArrayList<Ingredient> listeIngredientLoot = new ArrayList<Ingredient>();
 		listeIngredientLoot.add(pdtLoot);
 		ArrayList<Ingredient> listeIngredient = new ArrayList<Ingredient>();
-		
+
 		ArrayList<Ingredient> listeIngredient2= new ArrayList<Ingredient>();
 		listeIngredient2.add(pdt);
 		ArrayList<Ingredient> listeIngredientLoot2 = new ArrayList<Ingredient>();
 		listeIngredientLoot2.add(farineLoot);
 		listeIngredientLoot2.add(pommeLoots);
-		
+
 		ArrayList<Ingredient> listeIngredient3= new ArrayList<Ingredient>();
 		listeIngredient3.add(farine);
 		listeIngredient3.add(pommes);
@@ -83,15 +82,13 @@ public class DataInJava implements InterfaceData {
 		listeIngredientLoot3.add(farineLoot);
 		listeIngredientLoot3.add(pommeLoots);
 		listeIngredientLoot3.add(pdtLoot);
-		
-		
+
+
 		Recipe recette1 = new Recipe("tourte", 10, 10, 10, 5, listeIngredient , listeIngredientLoot); 
 		Recipe recette2 = new Recipe("frites", 12, 12, 12, 2, listeIngredient2, listeIngredientLoot2);
 		Recipe recette3 = new Recipe("cake", 15, 15, 16, 9, listeIngredient3, listeIngredientLoot3);
 
-	
-=======
->>>>>>> Stashed changes
+
 		// TODO Auto-generated method stub
 		recipes.add(recette1);
 		recipes.add(recette2);
@@ -105,9 +102,9 @@ public class DataInJava implements InterfaceData {
 		ArrayList<Lesson> lessons = new ArrayList<Lesson>();
 
 		Lesson lesson = new Lesson("Cuisinner les pommes", 10, 10, 5, 20, 300);
-		
+
 		lessons.add(lesson);
-		
+
 		return lessons;
 
 	}
@@ -115,7 +112,7 @@ public class DataInJava implements InterfaceData {
 	@Override
 	public void saveUser(User user) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
