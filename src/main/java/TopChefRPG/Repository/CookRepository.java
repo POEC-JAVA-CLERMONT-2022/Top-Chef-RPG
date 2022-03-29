0ee54f262a473ad1b318118de196c28c5ee8d049
manufrom63 <emmanuel.gicquel@gmail.com>
2022-03-29 14:05:34 +0200

    recipe gestion

--- src/main/java/TopChefRPG/Repository/CookRepository.java
+++ src/main/java/TopChefRPG/Repository/CookRepository.java
@@ -3,7 +3,9 @@ package TopChefRPG.Repository;
 import TopChefRPG.model.Cook;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CookRepository extends JpaRepository<Cook,Long> {
 
 
