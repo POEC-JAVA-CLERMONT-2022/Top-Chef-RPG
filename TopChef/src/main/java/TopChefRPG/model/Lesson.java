2c8f0399cd83d73f2442f5898104dcdfeba3b264
fabien <100273019+agalyn63@users.noreply.github.com>
2022-03-29 10:16:37 +0200

    Lesson and recipe

---
+++ TopChef/src/main/java/TopChefRPG/model/Lesson.java
@@ -0,0 +1,48 @@
+package TopChefRPG.model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long idLesson;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(firstIngredient = "firstIngredient")
+    private String firstIngredient;
+
+    @Column(firstQty = "firstQty")
+    private Integer firstQty;
+
+    @Column(secondIngredient = "secondIngredient")
+    private String thirdIngredient;
+
+    @Column(secondQty = "secondQty")
+    private Integer secondQty;
+
+    @Column(thirdIngredient = "thirdIngredient")
+    private String thirdIngredient;
+
+    @Column(thirdQty = "thirdQty")
+    private Integer thirdQty;
+
+    @Column(experience = "experience")
+    private Integer experience;
+
+    @Column(looting1Name = "looting1Name")
+    private String looting1Name;
+
+    @Column(looting1Qty = "looting1Qty")
+    private Integer looting1Qty;
+
+    @Column(looting2Name = "looting2Name")
+    private String looting2Name;
+
+    @Column(looting2Qty = "looting2Qty")
+    private Integer looting2Qty;
+}
