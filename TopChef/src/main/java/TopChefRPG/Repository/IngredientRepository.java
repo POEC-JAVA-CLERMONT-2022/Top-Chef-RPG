e103060148a9d86fe31e6fce8261f40092593ad6
manufrom63 <emmanuel.gicquel@gmail.com>
2022-03-29 12:13:52 +0200

    ajout get userbyId

--- TopChef/src/main/java/TopChefRPG/Repository/IngredientRepository.java
+++ TopChef/src/main/java/TopChefRPG/Repository/IngredientRepository.java
@@ -4,6 +4,5 @@ import TopChefRPG.model.Ingredient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface IngredientRepository extends JpaRepository<Ingredient,Long>{
 }
--- TopChef/src/main/java/TopChefRPG/Repository/UserRepository.java
+++ TopChef/src/main/java/TopChefRPG/Repository/UserRepository.java
@@ -2,11 +2,15 @@ package TopChefRPG.Repository;
 
 import TopChefRPG.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>
 {
     User findByName(String name);
+
+    Optional<User> findById(Long Id);
+
 }
