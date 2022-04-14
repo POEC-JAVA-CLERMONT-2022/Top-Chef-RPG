4dbf217d4f533437071e72fcfb60e54b8f32705e
fabien <100273019+agalyn63@users.noreply.github.com>
2022-04-14 12:11:47 +0200

    correction test unitaire

---
+++ src/test/java/TopChefRPG/Service/CookServiceTest.java
@@ -0,0 +1,51 @@
+
+package TopChefRPG.Service;
+
+import TopChefRPG.model.Cook;
+import TopChefRPG.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CookServiceTest {
+/*
+    @Test
+
+    @DisplayName("Save Cook")
+    public void testSaveCook() {
+
+        //instantiation donnée
+
+
+
+
+
+        //appel de la méthode à tester
+
+
+        cook1 = cookService.saveCook(cook1);
+        //cookService.saveCook(cook2);
+        System.out.println("toto)");
+        //test des valeurs après execution du code
+        //test si l'id de cook 1 = 1'
+        Assertions.assertEquals("alexandre", mockcookRepository.getCookById(cook1.getId()).getName());
+        //test du nombre de cook créés
+        //Assertions.assertEquals(2, cookRepository.findAll().size());
+
+    }
+}
+
+
+    @Test
+    @DisplayName("test create cook")
+    public void testCreateCook(){
+
+        User paulo = userService.create("paulo", "mail","password");
+
+
+        Cook cook1 = cookService.createCook("alexandre", 'M', paulo);
+
+        Assertions.assertNotNull(cook1);
+*/
+    }
+
