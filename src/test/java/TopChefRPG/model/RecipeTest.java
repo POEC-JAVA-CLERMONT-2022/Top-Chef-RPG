ba34d3ac6de4db4713c165f0b11e696e4149a228
agalyn63 <fabien.daudet@outlook.com>
2022-04-19 13:56:56 +0200

    cooklesson and recipe tests

--- src/test/java/TopChefRPG/model/RecipeTest.java
+++ src/test/java/TopChefRPG/model/RecipeTest.java
@@ -1,4 +1,5 @@
 package TopChefRPG.model;
+
 import TopChefRPG.Service.CookService;
 import TopChefRPG.Service.RecipeService;
 import TopChefRPG.Service.UserService;
@@ -9,15 +10,33 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import  TopChefRPG.model.CookLesson;
+import TopChefRPG.model.CookLesson;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 public class RecipeTest {
 
     @Test
     @DisplayName("constructeur recipe")
-    {
-        Recipe recipe = new Recipe("poutine","frites", 30, "érable", 50, "fromage", 60, 12, "pommes", )
+    public void createRecipe() {
+        Recipe recipe = new Recipe("poutine", "frites", 30, "érable", 50, "fromage", 60, 12, "pommes", 10, "patates", 10, 10, 11, 12);
+
+        Assertions.assertNotNull(recipe);
+        Assertions.assertEquals("poutine", recipe.getName());
+        Assertions.assertEquals("frites", recipe.getFirstIngredient());
+        Assertions.assertEquals(30, recipe.getFirstQty());
+        Assertions.assertEquals("érable", recipe.getSecondIngredient());
+        Assertions.assertEquals(50, recipe.getSecondQty());
+        Assertions.assertEquals("fromage", recipe.getThirdIngredient());
+        Assertions.assertEquals(60, recipe.getThirdQty());
+        Assertions.assertEquals(12, recipe.getExperience());
+        Assertions.assertEquals("pommes", recipe.getLooting1Name());
+        Assertions.assertEquals(10, recipe.getLooting1Qty());
+        Assertions.assertEquals("patates", recipe.getLooting2Name());
+        Assertions.assertEquals(10, recipe.getLooting2Qty());
+        Assertions.assertEquals(10, recipe.getStrengthRequired());
+        Assertions.assertEquals(12, recipe.getCreativityRequired());
+        Assertions.assertEquals(11, recipe.getDexterityRequired());
     }
 
 }
