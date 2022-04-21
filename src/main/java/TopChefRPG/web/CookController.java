a3c68fe98a8265905b66a8b84cc98c8d60365aa0
agalyn63 <fabien.daudet@outlook.com>
2022-04-21 16:24:51 +0200

    debut des logger

--- src/main/java/TopChefRPG/web/CookController.java
+++ src/main/java/TopChefRPG/web/CookController.java
@@ -6,6 +6,8 @@ import TopChefRPG.Service.DTO.CookDTO;
 import TopChefRPG.Service.UserService;
 import TopChefRPG.model.Cook;
 import TopChefRPG.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/cook")
 public class CookController {
+
+    Logger logger = LoggerFactory.getLogger(CookController.class);
+
     @Autowired
     CookService cookService;
 
@@ -22,30 +27,32 @@ public class CookController {
 
     //http://localhost:8080/cook/1
     @GetMapping("/{id}")
-    public CookDTO getCookById(@PathVariable int id)
-    {
+    public CookDTO getCookById(@PathVariable int id) {
+        logger.info("given cook {}", id);
         CookDTO cookDTO = new CookDTO();
         cookDTO.setCookDTO(cookService.getCookById(id));
         return cookDTO;
     }
 
     @GetMapping("/{id}/delete")
-    public void delCookById(@PathVariable int id)
-    {
+    public void delCookById(@PathVariable int id) {
+        logger.info("destruction du cook {} si il le veut bien, id");
         Cook cook = cookService.getCookById(id);
         cookService.delCookById(id);
     }
-    @PostMapping( "/changeName/{idCook}")
+
+    @PostMapping("/changeName/{idCook}")
     public void changeName(@RequestBody CookDTO cookDTO, @PathVariable int idCook) {
+        logger.info("changement du nom du cook {}", idCook);
         Cook cook = cookService.getCookById(idCook);
         cookService.changeName(cook, cookDTO.getNewName());
     }
 
     //TODO pas le bon controller
-    @PostMapping ("/createCook/{idUser}")
-    public void createCook(@RequestBody CookDTO cookDTO, @PathVariable int idUser)
-    {
+    @PostMapping("/createCook/{idUser}")
+    public void createCook(@RequestBody CookDTO cookDTO, @PathVariable int idUser) {
         User user = userService.findById(idUser);
         cookService.createCook(cookDTO.getName(), cookDTO.getGender(), user);
+        logger.info("Création d'un nouveau pour le User {}", user.getName());
     }
 }
